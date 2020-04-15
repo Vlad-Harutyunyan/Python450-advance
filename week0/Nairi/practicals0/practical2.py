d3c7845a3d36ad9acd8c428c239a92389c2ae5cc
Vlad <43403715+Vlad-Harutyunyan@users.noreply.github.com>
2020-04-15 19:43:02 +0300

    Add files via upload

--- week0/Nairi/practicals0/practical2.py
+++ week0/Nairi/practicals0/practical2.py
@@ -5,13 +5,31 @@ def type1(l):
     print(len(sets[0].intersection(*sets[1:])))
 
 def type2(l):
-    pass
+    first = [char for char in l[0]]
+    cnt = len(l) - 1
+    result = 0
 
+    for elem in range(len(first)):
+        temp = 0
+        for x in range(1,len(l)):
+            if first[elem] in l[x]: 
+                temp += 1
+        if temp == cnt :
+            result += 1
 
-
+    print(result)
 
 if __name__ == "__main__" :
-    l  = ['aaae','aaaeb','aaaecfc']
+    print('------- test 1 ------- ')
+    l  = ['aac','aacb','aac','aace']
+    type1(l)
+    type2(l)
+    print('------- test 2 ------- ')
+    l  = ['aac','aab','aae']
+    type1(l)
+    type2(l)
+    print('------- test 3 ------- ')
+    l  = ['a','b','c']
     type1(l)
     type2(l)
     
