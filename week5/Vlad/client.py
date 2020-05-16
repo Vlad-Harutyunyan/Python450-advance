6f4098e19bd897187e11e0bc79e8678bae3065c0
Vlad <43403715+Vlad-Harutyunyan@users.noreply.github.com>
2020-05-16 14:33:46 +0300

    Add files via upload

---
+++ week5/Vlad/client.py
@@ -0,0 +1,47 @@
+import socket
+import json
+
+IP = '127.0.0.1'
+PORT = 8027
+SIZE = 1024 # in bytes
+  
+client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+client.connect((IP,PORT))
+
+
+def printBoard(brd):
+    print(brd['1'][0] + '|' +brd['1'][1] + '|' + brd['1'][2])
+    print('-+-+-')
+    print(brd['2'][0] + '|' +brd['2'][1] + '|' + brd['2'][2])
+    print('-+-+-')
+    print(brd['3'][0] + '|' +brd['3'][1] + '|' + brd['3'][2])
+
+
+
+
+
+while True:
+    user_input = input('Possition : ')
+    
+    if user_input == 'exit':
+        client.sendall(user_input.encode())
+        break
+    elif not len(user_input):
+        print('empty')
+    else :
+        client.sendall(user_input.encode())
+        data =  json.loads(client.recv(SIZE).decode("utf-8"))
+        if '[error]' in data:
+            print(data)
+        if '[win]' in data :
+            print('You won')
+        elif '[lose]' in data :
+            print('You lose')
+        elif '[draw]' in data :
+            print('game end with draw !!!')
+        else:
+            printBoard(data)
+
+
+client.shutdown(socket.SHUT_RDWR)
+client.close()
---
+++ week5/Vlad/server.py
@@ -0,0 +1,85 @@
+import socket
+import json
+import random
+IP = '178.0.0.1'
+PORT = 8027
+SIZE = 1024 # in bytes
+
+
+
+board = {
+    1:[' ',' ',' '],
+    2:[' ',' ',' '],
+    3:[' ',' ',' '],
+}
+
+
+def CheckVictory(board, t):
+
+    if board[1][0]  == t and  board[1][1] == t and board[1][2] == t :
+        return True
+    elif board[2][0]  == t and  board[2][1] == t and board[2][2] == t :
+        return True
+    elif board[3][0]  == t and  board[3][1] == t and board[3][2] == t :
+        return True
+    elif board[1][0]  == t and  board[2][1] == t and board[3][2] == t :
+        return True 
+    elif board[1][0]  == t and  board[2][0] == t and board[3][0] == t :
+        return True
+    elif board[1][1]  == t and  board[2][1] == t and board[3][1] == t :
+        return True
+    elif board[1][2]  == t and  board[2][2] == t and board[3][2] == t :
+        return True
+    elif board[3][2]  == t and  board[2][1] == t and board[3][0] == t :
+        return True
+    return False
+
+
+def checkOver(board) :
+    for x in board:
+        if ' ' in board[x]:
+            return False
+    return True
+
+
+with  socket.socket(socket.AF_INET,socket.SOCK_STREAM) as sock: 
+    sock.bind(('',PORT))
+    sock.listen(4)
+    conn , addr = sock.accept()
+    print('Connected by ' , addr)
+    
+    with conn:
+        while True :
+            r = None
+            data = (conn.recv(SIZE).decode()).split(',')
+                
+            print(data)
+
+            if int(data[0]) > 3 :
+                r = json.dumps('[error] row index out of range !')
+            elif int(data[1]) >= 3 :
+                r = json.dumps('[error] col index out of range !')
+            elif board[int(data[0])] [int(data[1])] == 'X' or board[int(data[0])] [int(data[1])] == 'O' :
+                r = json.dumps('[error] this cell is busy')
+            elif CheckVictory(board,'O'):
+                r = json.dumps('[lose] O won !')
+            elif checkOver(board) :
+                r =  json.dumps('[draw] game end!')
+            else :
+                board[int(data[0])] [int(data[1])] = 'X'
+                if CheckVictory(board,'X'):
+                    r = json.dumps('[win] X won !')
+                else :  
+                    for x in range (20):
+                        rand_x = random.randint(1,3)
+                        rand_y = random.randint(0,2)
+                        if board[rand_x][rand_y] != 'X' and board[rand_x][rand_y] != 'O':
+                            board[rand_x][rand_y] = 'O'
+                            break
+                    r = json.dumps(board)
+                    
+
+
+            conn.sendall(r.encode())
+
+            
